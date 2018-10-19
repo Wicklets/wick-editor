cb7af19e002af25f389ab9725f100998c8cbb15c
Luca Damasco <luca.damasco@gmail.com>
2018-10-19 11:26:31 -0400

    Adding comments to differentiate controls for each panel.

--- src/Editor.js
+++ src/Editor.js
@@ -8,18 +8,24 @@ import { Button } from 'reactstrap';
 class Editor extends Component {
   render() {
     return (
+      // Menu Bar
       <SplitPane split="horizontal" minSize={50} maxSize={50}>
         <div className="pane-menuBar">MenuBar</div>
         <div>
+          // Toolbox
           <SplitPane split="vertical" minSize={50} maxSize={50}>
             <div className="pane-toolBox">ToolBox</div>
             <div>
+               // Inspector
                 <SplitPane split="vertical" minSize={300} maxSize={500} primary="second">
                   <div>
+                    // Timeline
                     <SplitPane split="horizontal" minSize={200} maxSize={350}>
                       <div className="pane-timeline">Timeline</div>
                       <div>
+                        // Code Editor
                         <SplitPane split="horizontal" minSize={50} maxSize={400} primary="second">
+                          // Canvas
                           <div className="pane-canvas">Canvas</div>
                           <div className="pane-codeEditor">Code Editor</div>
                         </SplitPane>
