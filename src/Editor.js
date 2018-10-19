58a8b0e5c4295a48d54611d862dcca5e75c5cb79
Luca Damasco <luca.damasco@gmail.com>
2018-10-18 23:44:48 -0400

    Created baseline window panes for each elements.

--- src/Editor.js
+++ src/Editor.js
@@ -6,15 +6,47 @@ import SplitPane from "react-split-pane";
 class Editor extends Component {
   render() {
     return (
-      <SplitPane split="vertical" minSize={50} maxSize={50}>
-        <div></div>
-        <SplitPane split="horizontal">
-            <div></div>
-            <div></div>
-        </SplitPane>
+
+      <SplitPane split="horizontal" minSize={50} maxSize={50}>
+        <div className="pane-menuBar">MenuBar</div>
+        <div>
+          <SplitPane split="vertical" minSize={50} maxSize={50}>
+            <div className="pane-toolBox">ToolBox</div>
+            <div>
+                <SplitPane split="vertical" minSize={300} maxSize={500} primary="second">
+                  <div>
+                    <SplitPane split="horizontal" minSize={200} maxSize={350}>
+                      <div className="pane-timeline">Timeline</div>
+                      <div>
+                        <SplitPane split="horizontal" minSize={50} maxSize={400} primary="second">
+                          <div className="pane-canvas">Canvas</div>
+                          <div className="pane-codeEditor">Code Editor</div>
+                        </SplitPane>
+                      </div>
+                    </SplitPane>
+                  </div>
+                  <div className="pane-inspector">Inspector</div>
+                </SplitPane>
+            </div>
+          </SplitPane>
+        </div>
       </SplitPane>
-    )
+    );
   }
 }
 
 export default Editor
+
+// <div>Pane 1</div>
+// <SplitPane split="vertical" minSize={400} maxSize={500} defaultSize="50%">
+//   <div>Pane 2</div>
+//   <SplitPane split="vertical" >
+//     <SplitPane split="horizontal">
+//       <div>Pane 4</div>
+//       <SplitPane split="horizontal">
+//         <div>Pane 5</div>
+//       </SplitPane>
+//     </SplitPane>
+//     <div>Pane 3</div>
+//   </SplitPane>
+// </SplitPane>
