ce9f049e53cacffdcb138bfdbef9b205efa303d6
Luca Damasco <luca.damasco@gmail.com>
2018-10-19 16:03:16 -0400

    Adding resources for tool icons.

--- src/Editor/Components/Inspector/Inspector.jsx
+++ src/Editor/Components/Inspector/Inspector.jsx
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import './_inspector.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// component imports
 import DockedTitle from '../Util/DockedTitle/DockedTitle';
+
+// icon imports
+import iconSettings from '../../../resources/inspector-icons/settings.png';
 
 class Inspector extends Component {
   render() {
     return(
       <div className="inspector">
         <DockedTitle title={"Inspector"}></DockedTitle>
+        <div className="selection-name-container">
+          <img className="selection-icon" src={iconSettings}></img>
+          <div className="selection-name">Inspector</div>
+        </div>
       </div>
 
     )
