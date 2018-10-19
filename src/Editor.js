6c5c493f155c57117d9f0c7a1a5dd9802f1a5894
Luca Damasco <luca.damasco@gmail.com>
2018-10-18 21:14:45 -0400

    Cleaning up base file.

---
+++ src/Editor.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Editor.css';
+
+class Editor extends Component {
+  render() {
+    return (
+      <div className="wick-editor">
+
+      </div>
+    )
+  }
+}
+
+export default Editor
