bfb76d908c1c81e8c9ab221c574ed5c465add3d6
Luca Damasco <luca.damasco@gmail.com>
2018-10-19 12:44:41 -0400

    Create basic app file structure.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Editor from './Editor';
+import Editor from './Editor/Editor';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Editor />, document.getElementById('root'));
