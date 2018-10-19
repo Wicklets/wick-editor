00cb6f5577af899c183fb77236a8cf83d7be2507
Luca Damasco <luca.damasco@gmail.com>
2018-10-18 21:41:43 -0400

    Create styles folder and include split pan code.

--- src/Editor.js
+++ src/Editor.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Editor.css';
+import './styles/split-pane.css'
 import SplitPane from "react-split-pane";
 
 class Editor extends Component {
   render() {
     return (
-      <SplitPane split="vertical" minSize={50}>
+      <SplitPane split="vertical" minSize={50} maxSize={50}>
         <div></div>
         <SplitPane split="horizontal">
             <div></div>
