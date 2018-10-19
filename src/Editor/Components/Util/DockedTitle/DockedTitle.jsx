072a18ee74697602e70f4a0dd2c76fd3dd4faf88
Luca Damasco <luca.damasco@gmail.com>
2018-10-19 13:30:17 -0400

    Created DockedTitle Component

---
+++ src/Editor/Components/Util/DockedTitle/DockedTitle.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './DockedTitle.css';
+
+class DockedTitle extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default DockedTitle
