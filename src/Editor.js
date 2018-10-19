6beab2073e7cd012c5781c14310299ae05c8f250
Zach Rispoli <zach.rispoli@gmail.com>
2018-10-18 21:32:24 -0400

    add working react-split-pane component

--- src/Editor.js
+++ src/Editor.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Editor.css';
+import SplitPane from "react-split-pane";
 
 class Editor extends Component {
   render() {
     return (
-      <SplitPane split="vertical" minSize={50} defaultSize={100}>
+      <SplitPane split="vertical" minSize={50}>
         <div></div>
-        <div></div>
+        <SplitPane split="horizontal">
+            <div></div>
+            <div></div>
+        </SplitPane>
       </SplitPane>
     )
   }
