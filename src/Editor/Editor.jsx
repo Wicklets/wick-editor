c72ed36a8c4ebfd88ef860530b786ff903fd08bf
Zach Rispoli <zach.rispoli@gmail.com>
2018-10-21 13:15:39 -0400

    Create skeleton for wick editor panels using ReFlex

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -22,7 +22,6 @@ import CodeEditor from './Panels/CodeEditor/CodeEditor';
 class Editor extends Component {
 
     constructor () {
-
       super()
 
       this.resizeProps = {
@@ -32,17 +31,13 @@ class Editor extends Component {
     }
 
     onResize (e) {
-
       if (e.domElement) {
-
         e.domElement.classList.add('resizing')
       }
     }
 
     onStopResize (e) {
-
       if (e.domElement) {
-
         e.domElement.classList.remove('resizing')
       }
     }
@@ -52,50 +47,30 @@ class Editor extends Component {
         <ReflexContainer orientation="horizontal">
 
           <ReflexElement className="header" flex={0.05}>
-            <div className="pane-content">
-              <label>
-                Header (fixed)
-              </label>
-            </div>
+            <div className="pane-content">Header (Fixed)</div>
           </ReflexElement>
 
           <ReflexElement>
             <ReflexContainer orientation="vertical">
               <ReflexElement flex={0.05} {...this.resizeProps}>
-                <div className="left-sidebar">
-                  <label>
-                    Sidebar
-                  </label>
-                </div>
+                <div className="pane-content">Sidebar (Fixed)</div>
               </ReflexElement>
               <ReflexElement {...this.resizeProps}>
                 <ReflexContainer orientation="horizontal">
-                  <ReflexElement {...this.resizeProps}>
-                    <div className="pane-content">
-                      <label>
-                        Center Pane <br/> Top
-                      </label>
-                    </div>
+                  <ReflexElement flex={0.2} {...this.resizeProps}>
+                    <div className="pane-content">Center Pane Top</div>
                   </ReflexElement>
 
                   <ReflexSplitter {...this.resizeProps}/>
 
                   <ReflexElement {...this.resizeProps}>
-                    <div className="pane-content">
-                      <label>
-                        Center Pane <br/> Middle
-                      </label>
-                    </div>
+                    <div className="pane-content">Center Pane Middle</div>
                   </ReflexElement>
 
                   <ReflexSplitter {...this.resizeProps}/>
 
-                  <ReflexElement {...this.resizeProps}>
-                    <div className="pane-content">
-                      <label>
-                        Center Pane <br/> Bottom
-                      </label>
-                    </div>
+                  <ReflexElement flex={0.2} {...this.resizeProps}>
+                    <div className="pane-content">Center Pane Bottom</div>
                   </ReflexElement>
                 </ReflexContainer>
               </ReflexElement>
@@ -105,29 +80,23 @@ class Editor extends Component {
               <ReflexElement flex={0.2} {...this.resizeProps}>
                 <ReflexContainer orientation="horizontal">
                   <ReflexElement {...this.resizeProps}>
-                    <div className="pane-content">
-                      <label>
-                        Right Pane <br/> Bottom
-                      </label>
-                    </div>
+                    <div className="pane-content">Right Pane Top</div>
                   </ReflexElement>
 
                   <ReflexSplitter {...this.resizeProps}/>
 
                   <ReflexElement {...this.resizeProps}>
-                    <div className="pane-content">
-                      <label>
-                        Right Pane <br/> Bottom
-                      </label>
-                    </div>
+                    <div className="pane-content">Right Pane Bottom</div>
                   </ReflexElement>
                 </ReflexContainer>
               </ReflexElement>
+
             </ReflexContainer>
           </ReflexElement>
         </ReflexContainer>
       )
   }
+
 }
 
 export default Editor
