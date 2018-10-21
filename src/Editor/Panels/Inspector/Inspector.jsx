3193a587a948717662596cb6a1702fa754eddfd8
Luca Damasco <luca.damasco@gmail.com>
2018-10-21 15:29:04 -0400

    Update the inspector title to a component.

--- src/Editor/Panels/Inspector/Inspector.jsx
+++ src/Editor/Panels/Inspector/Inspector.jsx
@@ -3,18 +3,14 @@ import './_inspector.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import DockedTitle from 'Editor/Util/DockedTitle/DockedTitle';
-
-import iconSettings from '../../../resources/inspector-icons/settings.png';
+import InspectorTitle from './InspectorTitle/InspectorTitle';
 
 class Inspector extends Component {
   render() {
     return(
       <div className="docked-pane inspector">
         <DockedTitle title={"Inspector"}></DockedTitle>
-        <div className="selection-name-container">
-          <img className="selection-icon" alt="" src={iconSettings}></img>
-          <div className="selection-name">Inspector</div>
-        </div>
+        <InspectorTitle />
       </div>
 
     )
