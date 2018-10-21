146c59f99f8a1b40a41ad0cd9445a1724da393eb
Zach Rispoli <zach.rispoli@gmail.com>
2018-10-21 15:05:18 -0400

    Timeline and paper.js canvases resize correctly

--- src/Editor/Panels/Canvas/Canvas.jsx
+++ src/Editor/Panels/Canvas/Canvas.jsx
@@ -3,18 +3,17 @@ import './_canvas.scss';
 
 class Canvas extends Component {
   componentDidMount() {
-    console.log(window.paper);
     window.paper.setup(this.refs.canvas);
     var p = new window.paper.Path.Circle(new window.paper.Point(30,30), 50);
     p.fillColor = 'blue';
     p.strokeColor = 'red';
     p.strokeWidth = 5;
     window.onresize = function () {
-      var widthDiff = paper.view.viewSize.width - $('.paper-canvas-container').width();
-      var heightDiff = paper.view.viewSize.height - $('.paper-canvas-container').height();
-      paper.view.viewSize.width = $('.paper-canvas-container').width();
-      paper.view.viewSize.height = $('.paper-canvas-container').height();
-      paper.view.center = paper.view.center.add(new window.paper.Point(widthDiff/2/window.paper.view.zoom, heightDiff/2/window.paper.view.zoom))
+      var widthDiff = window.paper.view.viewSize.width - window.$('.paper-canvas-container').width();
+      var heightDiff = window.paper.view.viewSize.height - window.$('.paper-canvas-container').height();
+      window.paper.view.viewSize.width = window.$('.paper-canvas-container').width();
+      window.paper.view.viewSize.height = window.$('.paper-canvas-container').height();
+      window.paper.view.center = window.paper.view.center.add(new window.paper.Point(widthDiff/2/window.paper.view.zoom, heightDiff/2/window.paper.view.zoom))
     }
   }
 
