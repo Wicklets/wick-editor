0404397fd14d6ca6caecd3e72f84e8bbf3751c1a
Luca Damasco <luca.damasco@gmail.com>
2018-10-23 13:51:49 -0400

    Asset library initial implementation.

--- src/Editor/Panels/AssetLibrary/AssetLibrary.jsx
+++ src/Editor/Panels/AssetLibrary/AssetLibrary.jsx
@@ -2,25 +2,55 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import DockedTitle from 'Editor/Util/DockedTitle/DockedTitle';
+import { Button } from 'reactstrap';
 
 import './_assetlibrary.scss';
+
+// import iconAdd from 'resources/asset-library-icons/add.svg';
+// import iconEdit from 'resources/asset-library-icons/edit.png';
+// import iconDelete from 'resources/asset-library-icons/delete.svg';
 
 class AssetLibrary extends Component {
   constructor(props) {
     super(props);
     this.state ={
       assets: [
-        { name:"a" },
-        { name:"b" },
-        { name:"c" },
+        { name:"a1", uuid:"1"},
+        { name:"b2", uuid:"2"},
+        { name:"c3", uuid:"3"},
+        { name:"a4", uuid:"4"},
+        { name:"b5", uuid:"5"},
+        { name:"c6", uuid:"6"},
       ]
     }
     this.makeNode = this.makeNode.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleDelete(uuid) {
+    console.log("DEL", uuid);
+  }
+
+  handleAdd(uuid) {
+    console.log("ADD", uuid);
+  }
+
+  handleEdit(uuid) {
+    console.log("EDIT", uuid);
   }
 
   makeNode(asset, i) {
     return (
-      <div className="asset-node">{asset.name}</div>
+      <div key={i} className="asset-node">
+        <div className="assetName">{asset.name}</div>
+        <div className="btn-container">
+          <Button className="btn-asset-action" onClick={() => this.handleDelete(asset.uuid)}>DEL</Button>
+          <Button className="btn-asset-action" onClick={() => this.handleAdd(asset.uuid)}>ADD</Button>
+          <Button className="btn-asset-action" onClick={() => this.handleEdit(asset.uuid)}>EDIT</Button>
+        </div>
+      </div>
     )
   }
 
