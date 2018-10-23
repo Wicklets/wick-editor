80c8d663bee695e46a7ca8b42e0758d4add55fe6
Luca Damasco <luca.damasco@gmail.com>
2018-10-22 22:21:15 -0400

    Add basic asset library.

--- src/Editor/Panels/AssetLibrary/AssetLibrary.jsx
+++ src/Editor/Panels/AssetLibrary/AssetLibrary.jsx
@@ -6,10 +6,31 @@ import DockedTitle from 'Editor/Util/DockedTitle/DockedTitle';
 import './_assetlibrary.scss';
 
 class AssetLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      assets: [
+        { name:"a" },
+        { name:"b" },
+        { name:"c" },
+      ]
+    }
+    this.makeNode = this.makeNode.bind(this);
+  }
+
+  makeNode(asset, i) {
+    return (
+      <div className="asset-node">{asset.name}</div>
+    )
+  }
+
   render() {
     return(
       <div className="docked-pane asset-library">
         <DockedTitle title={"Asset Library"}></DockedTitle>
+        <div className="asset-container">
+          {this.state.assets.map(this.makeNode)}
+        </div>
       </div>
     )
   }
