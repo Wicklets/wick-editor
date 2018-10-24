75724150df7ef955ca5e7f21e9da898efc947bff
Luca Damasco <luca.damasco@gmail.com>
2018-10-24 04:18:43 -0400

    Componentize the tool icons.

--- src/Editor/Util/ToolIcon/ToolIcon.jsx
+++ src/Editor/Util/ToolIcon/ToolIcon.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './_toolbutton.scss'
-
-
-import ToolIcon from 'Editor/Util/ToolIcon/ToolIcon';
 
 import iconBrush from 'resources/tool-icons/brush.svg';
 import iconCursor from 'resources/tool-icons/cursor.svg';
@@ -16,12 +12,13 @@ import iconEraser from 'resources/tool-icons/eraser.svg';
 import iconPan from 'resources/tool-icons/pan.svg';
 import iconZoom from 'resources/tool-icons/zoom.svg';
 
-class ToolButton extends Component {
+class ToolIcon extends Component {
   constructor(props) {
     super(props);
 
     this.icons = {
       "croquisBrush":iconBrush,
+      "brush":iconBrush,
       "cursor":iconCursor,
       "ellipse":iconEllipse,
       "rectangle":iconRectangle,
@@ -36,16 +33,10 @@ class ToolButton extends Component {
   }
 
   render() {
-    return(
-      <div
-        type="button"
-        className={this.props.toolIsActive(this.props.name) ? "tool-button active-tool" : "tool-button"}
-        onClick={() => {this.props.activateTool(this.props.name)}}
-        >
-        <ToolIcon name={this.props.name} />
-      </div>
+    return (
+      <img className="img-tool-icon" src={this.icons[this.props.name]} />
     )
   }
 }
 
-export default ToolButton
+export default ToolIcon
