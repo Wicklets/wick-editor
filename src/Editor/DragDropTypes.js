6428be06210e710f110ac994a243307fed1a7a21
Luca Damasco <luca.damasco@gmail.com>
2018-11-27 15:43:18 -0500

    Add types file for drag drop

---
+++ src/Editor/DragDropTypes.js
@@ -0,0 +1,5 @@
+export default {
+  ASSET: 'Asset', // TODO: Convert to function which outputs string based on id.
+  CANVAS: ['Asset'], // TODO: Should take in all ids that canvas can receive.
+  TIMELINE: ['Sound',] // TODO: Should take in all ids that timeline can receive.
+}
