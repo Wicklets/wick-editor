929456cf6838b386fabdd1723e2781dc98606293
Zach Rispoli <zach.rispoli@gmail.com>
2018-11-28 16:54:40 -0500

    Fix NumericInput style issue

--- src/Editor/Util/WickInput/WickInput.jsx
+++ src/Editor/Util/WickInput/WickInput.jsx
@@ -30,8 +30,8 @@ class WickInput extends Component {
   render() {
     if (this.props.type==="numeric") {
       return (
-        //eslint-disable-next-line
         <NumericInput
+          style={false}
           className="wick-input"
           {...this.props}
           ></NumericInput>
