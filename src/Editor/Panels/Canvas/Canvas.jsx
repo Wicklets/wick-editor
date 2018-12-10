3c94ad55d93c90f7c2c29933b079429a28b454cc
Luca Damasco <luca.damasco@gmail.com>
2018-12-09 20:11:12 -0500

    Fix Canvas update

--- src/Editor/Panels/Canvas/Canvas.jsx
+++ src/Editor/Panels/Canvas/Canvas.jsx
@@ -60,11 +60,6 @@ class Canvas extends Component {
     this.onSelectionChanged = this.onSelectionChanged.bind(this);
 
     this.canvasContainer = React.createRef();
-    console.log("Bleh");
-  }
-
-  shouldComponentUpdate() {
-    return false; 
   }
 
   componentDidMount() {
