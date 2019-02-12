de47857b15532fab183115b2fd8d016876079b67
Luca Damasco <luca.damasco@gmail.com>
2019-02-12 18:15:22 -0500

    Update Toolbox with separators.

---
+++ src/Editor/Panels/Toolbox/ToolboxBreak/ToolboxBreak.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './_toolboxbreak.scss';
+
+class ToolboxBreak extends Component {
+  render() {
+    return (
+      <div className="toolbox-break"></div>
+    )
+  }
+}
+
+export default ToolboxBreak
