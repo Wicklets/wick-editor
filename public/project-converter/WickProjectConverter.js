a915bcbc75568b86f4a8cee69a4dc6f1d26d8476
Zach Rispoli <zach.rispoli@gmail.com>
2019-03-04 16:34:35 -0800

    More work on project converter

--- public/project-converter/WickProjectConverter.js
+++ public/project-converter/WickProjectConverter.js
@@ -1,5 +1,44 @@
 // Wick Alpha 15.2 -> Wick Bagel 1.0 project converter
 
-function convertProject (project) {
-	console.log(project)
+function convertProjectFile (projectFile, callback) {
+    var reader = new FileReader();
+    var filename = projectFile.name;
+    reader.onload = function(e) {
+        var arrayBuffer = reader.result;
+        var byteArray = new Uint8Array(arrayBuffer);
+        var wickProjectJSON = LZString.decompressFromUint8Array(byteArray);
+        
+        var project = WickProject.fromJSON(wickProjectJSON);
+        var filenameParts = filename.split('-');
+        var name = filenameParts[0];
+        if(name.includes('.json')) {
+            name = name.split('.json')[0];
+        }
+        if(name.includes('.wick')) {
+            name = name.split('.wick')[0];
+        }
+        project.name = name || 'New Project';
+
+        convertProject(project, new Wick.Project(), callback);
+    };
+    reader.readAsArrayBuffer(projectFile);
+}
+
+function convertProject (project, converted, callback) {
+    var converted = new Wick.Project();
+    converted.backgroundColor = project.backgroundColor;
+    converted.framerate = project.framerate;
+    converted.width = project.width;
+    converted.height = project.height;
+
+    // TODO asset library
+
+    convertClip(project.rootObject, project.root);
+
+    callback(converted);
+}
+
+function convertClip (clip, converted) {
+    console.log(clip)
+    return converted;
 }
