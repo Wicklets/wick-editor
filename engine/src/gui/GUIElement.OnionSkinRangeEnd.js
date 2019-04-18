8cfd15f4e0af475dde02f36d2c07cd7ded96dd1a
Zach Rispoli <zach.rispoli@gmail.com>
2019-04-18 14:44:39 -0400

    Fix onion skin range end slider

--- engine/src/gui/GUIElement.OnionSkinRangeEnd.js
+++ engine/src/gui/GUIElement.OnionSkinRangeEnd.js
@@ -41,7 +41,7 @@ Wick.GUIElement.OnionSkinRangeEnd = class extends Wick.GUIElement.OnionSkinRange
      */
     get x () {
         var project = this.model.project;
-        var x = (project.activeTimeline.playheadPosition - project.onionSkinSeekForwards + 1) * this.gridCellWidth;
+        var x = (project.activeTimeline.playheadPosition + project.onionSkinSeekForwards - 1) * this.gridCellWidth;
         x += this.gridCellWidth;
         x -= Wick.GUIElement.OnionSkinRange.DEFAULT_HANDLE_WIDTH;
         x += this.dragOffset * this.gridCellWidth;
@@ -59,7 +59,8 @@ Wick.GUIElement.OnionSkinRangeEnd = class extends Wick.GUIElement.OnionSkinRange
      *
      */
     drop () {
-        this.model.project.onionSkinSeekForwards = project.activeTimeline.playheadPosition - Math.floor(this.x / this.gridCellWidth) + 1;
+        this.model.project.onionSkinSeekForwards = project.activeTimeline.playheadPosition + Math.floor(-this.x / this.gridCellWidth);
+        this.model.project.onionSkinSeekForwards *= -1;
         this.dragOffset = 0;
     }
 
