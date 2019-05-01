f71a16a73d764d52969eea5c8b4dd03f07b72605
Zach Rispoli <zach.rispoli@gmail.com>
2019-05-01 13:00:39 -0400

    add legal header for new paper selection

--- engine/src/view/paper-ext/Paper.SelectionGUI.js
+++ engine/src/view/paper-ext/Paper.SelectionGUI.js
@@ -1,4 +1,31 @@
-paper.SelectionGUI = class {
+/*
+ * Copyright 2018 WICKLETS LLC
+ *
+ * This file is part of Wick Engine.
+ *
+ * Wick Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wick Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wick Engine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ /**
+  * This is a utility class for creating a selection box GUI. this will give you:
+  *  - a bounding box
+  *  - handles for scaling
+  *  - hotspots for rotating
+  *  - ...and much more!
+  */
+
+ paper.SelectionGUI = class {
     static get BOX_STROKE_WIDTH () {
         return 1;
     }
