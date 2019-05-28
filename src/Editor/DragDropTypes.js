759505482b4ab64213712cda4f4cd556c1ed83cc
Luca Damasco <luca.damasco@gmail.com>
2019-05-27 23:40:25 -0400

    Upgrade drag and drop to only allow proper images and assets onto canvas.

--- src/Editor/DragDropTypes.js
+++ src/Editor/DragDropTypes.js
@@ -1,5 +1,8 @@
 export default {
-  ASSET: 'Asset', // TODO: Convert to function which outputs string based on id.
-  CANVAS: ['Asset'], // TODO: Should take in all ids that canvas can receive.
-  TIMELINE: ['Sound',] // TODO: Should take in all ids that timeline can receive.
+  GET_ASSET_TYPE: (props) => {
+    if (props.asset) return props.asset.classname;
+    return 'Asset'
+  },
+  CANVAS: ['ImageAsset', 'ButtonAsset', 'ClipAsset'], // TODO: Should take in all ids that canvas can receive.
+  TIMELINE: ['SoundAsset'] // TODO: Should take in all ids that timeline can receive.
 }
