3239fcd80fb62af9b6db1fca8039f6192d2e4632
Luca Damasco <luca.damasco@gmail.com>
2019-11-25 10:14:25 -0500

    update hex code of transparent swatch.

--- src/Editor/Util/ColorPicker/WickColorPicker.jsx
+++ src/Editor/Util/ColorPicker/WickColorPicker.jsx
@@ -123,7 +123,7 @@ class WickColorPicker extends Component {
             }
         }
 
-        let colors = ['#D0021B', '#F5A623', '#F8E71C', '#8B572A', '#7ED321', '#417505', '#BD10E0', '#9013FE', '#4A90E2', '#50E3C2', '#B8E986', '#000000', '#4A4A4A', '#9B9B9B', '#FFFFFF', 'rgba(0,0,0,0)']
+        let colors = ['#D0021B', '#F5A623', '#F8E71C', '#8B572A', '#7ED321', '#417505', '#BD10E0', '#9013FE', '#4A90E2', '#50E3C2', '#B8E986', '#000000', '#4A4A4A', '#9B9B9B', '#FFFFFF', '#FFFFFF00']
         let lastUsedColorsDefaults = ["#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000"]
         let lastColors = this.props.lastColorsUsed || lastUsedColorsDefaults;
         return (
