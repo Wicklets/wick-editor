124fd01335eae9f2fa1801f6065a4430f9bc5eed
Zach Rispoli <zach.rispoli@gmail.com>
2019-11-26 16:14:32 -0500

    styling for builtin asset window

--- src/Editor/Modals/BuiltinLibrary/BuiltinLibrary.jsx
+++ src/Editor/Modals/BuiltinLibrary/BuiltinLibrary.jsx
@@ -20,56 +20,123 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-modal';
-import ActionButton from 'Editor/Util/ActionButton/ActionButton';
+import TabbedInterface from 'Editor/Util/TabbedInterface/TabbedInterface';
 
 import './_builtinlibrary.scss';
 
-import nightImage from 'resources/interface-images/blue_night.svg';
-
 class BuiltinLibrary extends Component {
+  static get ROOT_ASSET_PATH () {
+    return process.env.PUBLIC_URL + '/builtinlibrary/';
+  }
+
   get builtinAssets () {
     return [
       {
-        file: 'wickobjects/vcam.wickobj',
-        name: 'Vcam',
+        name: 'Clips',
+        assets: [{
+          file: 'wickobjects/vcam.wickobj',
+          name: 'Vcam',
+          icon: '',
+        },{
+          file: 'wickobjects/checkbox.wickobj',
+          name: 'Checkbox',
+          icon: '',
+        },{
+          file: 'wickobjects/keyboardcontrol.wickobj',
+          name: 'Keyboard Controlled Character',
+          icon: '',
+        },{
+          file: 'wickobjects/link.wickobj',
+          name: 'URL Link',
+          icon: '',
+        },{
+          file: 'wickobjects/slider.wickobj',
+          name: 'Slider',
+          icon: '',
+        },{
+          file: 'wickobjects/textinput.wickobj',
+          name: 'Text Input',
+          icon: '',
+        }],
       },
       {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
-      },
-      {
-        file: 'sounds/jason-derulo.mp3',
-        name: 'Jason Derulo',
+        name: 'Sounds',
+        assets: [{
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        },
+        {
+          file: 'sounds/jason-derulo.mp3',
+          name: 'Jason Derulo',
+          icon: 'icons/derulo.jpeg',
+        }]
       },
     ]
   }
 
   importAsset = (assetPath, assetName) => {
-    var path = process.env.PUBLIC_URL + '/builtinlibrary/' + assetPath;
+    var path = BuiltinLibrary.ROOT_ASSET_PATH + assetPath;
     console.log(path);
 
     fetch (path)
@@ -95,23 +162,45 @@ class BuiltinLibrary extends Component {
       onRequestClose={this.props.toggle}
       className="modal-body welcome-modal-body"
       overlayClassName="modal-overlay welcome-modal-overlay">
-        <div class='builtin-library-asset-grid'>
-          {
-            this.builtinAssets.map(asset => {
-              return this.renderBuiltinAsset(asset)
-            })
-          }
+        <div className='builtin-library'>
+          <div className="builtin-library-modal-title">
+            Builtin Library
+          </div>
+          <TabbedInterface tabNames={["Clips", "Sounds"]} >
+            {this.builtinAssets.map(assetGroup => {
+              return this.renderAssetGroup(assetGroup.name, assetGroup.assets)
+            })}
+          </TabbedInterface>
         </div>
       </Modal>
     );
   }
 
+  renderAssetGroup (name, assets) {
+    return (
+      <div className='builtin-library-asset-grid'>
+        {
+          assets.map(asset => {
+            return this.renderBuiltinAsset(asset)
+          })
+        }
+      </div>
+    )
+  }
+
   renderBuiltinAsset = (asset) => {
     return (
       <div
-        class='builtin-library-asset'
+        className='builtin-library-asset'
         onClick={(() => this.importAsset(asset.file, asset.name))}>
-        {asset.name}
+        <div className='builtin-library-asset-icon-container'>
+          <img
+            src={BuiltinLibrary.ROOT_ASSET_PATH + asset.icon}
+            className='builtin-library-asset-icon'/>
+        </div>
+        <div className='builtin-library-asset-name'>
+          {asset.name}
+        </div>
       </div>
     );
   }
