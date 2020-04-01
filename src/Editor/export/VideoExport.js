c0aee93f0880fe399766a5219858528a4800342b
Luca Damasco <luca.damasco@gmail.com>
2020-04-01 13:05:16 -0400

    Update video renderer to use new ffmpegjs.

--- src/Editor/export/VideoExport.js
+++ src/Editor/export/VideoExport.js
@@ -1,179 +1,103 @@
+const { createWorker } = require('@ffmpeg/ffmpeg');
 var b64toBuff = require('base64-arraybuffer');
 var toWav = require('audiobuffer-to-wav')
 
-class FFMPEG {
-  get onProgress () {
-      return this._onProgress || (() => {});
-  }
-
-  set onProgress (onProgress) {
-      this._onProgress = onProgress;
-  }
-
-  get onError () {
-    return this._onError || (() => {});
-  }
-
-  set onError (onError) {
-    this._onError = onError;
-  }
-
-  get onDone () {
-      return this._onDone || (() => {});
-  }
-
-  set onDone (onDone) {
-      this._onDone = onDone;
-  }
-
-  constructor () {
-    this._isReady = false;
-
-    this._worker = new Worker("corelibs/video/worker-asm.js");
-    this._worker.onmessage = (e) => {
-      var msg = e.data;
-      switch (msg.type) {
-        case "ready":
-          this._isReady = true;
-          break;
-        case "run":
-          break;
-        case "stdout":
-          //console.log(msg.data);
-          break;
-        case "stderr":
-          break;
-        case "done":
-          this.onDone(msg.data[0].data);
-          break;
-        case "exit":
-          this._worker.terminate();
-          break;
-        case "error":
-          this.onError(msg.data[0].data);
-          break;
-        default:
-          break;
-      }
-    };
-  }
-
-  run (ffmpegArgs, workerMemoryFiles, commandName) {
-    this._waitUntilReady(() => {
-      this._worker.postMessage({
-        type: "command",
-        arguments: ffmpegArgs,
-        files: workerMemoryFiles,
-        commandName: 'video_render',
-      });
-    });
-  }
-
-  _waitUntilReady (callback) {
-    var waitUntilReadyInterval = setInterval(() => {
-      if(this._isReady) {
-        clearInterval(waitUntilReadyInterval);
-        callback();
-      }
-    }, 10);
-  }
-}
-
 class VideoExport {
-  static renderVideo (args) {
-    if(!args.project) console.error('VideoExport: project is required.');
-    if(!args.onProgress) console.error('VideoExport: onProgress is required.');
-    if(!args.onFinish) console.error('VideoExport: onFinish is required.');
-
-    args.onProgress('Rendering video frames', 10);
-    this._generateVideoFile(args, videoFiles => {
-      args.onProgress('Rendering audio track', 33);
-      this._generateAudioFiles(args.project, audioFiles => {
-        args.onProgress('Mixing video and audio', 66);
-        this._mixAudioAndVideoFiles(args.project, videoFiles, audioFiles, videoWithAudio => {
-          args.onProgress('Finished video export', 100);
-          this._saveVideoFile(args.project, videoWithAudio);
-          args.onFinish();
-        });
+    /**
+     * project, onProgress, onError, onFinish;
+     */
+    static renderVideo = async (args) => {
+      const worker = createWorker({
+          logger: ({ message }) => console.log(message),
       });
-    })
-  }
 
-  static get _ffmpeg () {
-    // lazily create ffmpeg instance
-    if(!this._ffmpegInstance) {
-      this._ffmpegInstance = new FFMPEG();
+      args.worker = worker;
+
+      await worker.load();
+      let audio = await VideoExport._generateAudioFile(args);
+      let images = await VideoExport._generateProjectImages(args);
+      await VideoExport._generateVideo({images:images, audio:audio, args});
     }
-    return this._ffmpegInstance;
-  }
 
-  static _generateImageFiles (args, callback) {
-    let project = args.project;
-    var imageFiles = [];
+    static _generateAudioFile = async (args) => {
 
-    // Get frame images from project, send to the video worker.
-    let frameNumber = 0;
+      let {project, onProgress} = args;
 
-    project.generateImageSequence({
-      imageType: 'image/jpeg',
-      width: args.width,
-      height: args.height,
-      onProgress: (currentFrame, numTotalFrames) => {
-        let message = 'Rendering Frame: ' + currentFrame + '/' + numTotalFrames; 
-        let progress = currentFrame/numTotalFrames;
-        if (args.onProgress) {
-          args.onProgress(message, progress * 33);
-        }
-      },
-      onFinish: images => {
-        // Load frame images into the web worker's memory
-        images.forEach(image => {
-          // Create Name and array buffer of frame image.
-          let paddedNum = (frameNumber + '').padStart(12, '0');
-          let name = "frame" + paddedNum + ".jpeg";
+      onProgress && onProgress('Generating Audio Track...', 10);
 
-          // Get the base 64 value and convert it to an array buffer.
-          let cleanBase64 = image.src.split(',')[1];
-          let buffer = b64toBuff.decode(cleanBase64);
+      return new Promise (resolve => {
+          project.generateAudioTrack({}, audioBuffer => {
+              if(!audioBuffer) {
+                resolve();
+              } else {
+                var wavBuffer = toWav(audioBuffer);
+                resolve(new Uint8Array(wavBuffer));
+              }
+            });
+      });
+    }
 
-          // Store name and buffer in memfs appropriate object.
-          let memfs_obj = {name: name, data:new Uint8Array(buffer)};
+    /**
+     * Stores all frames of the project in the ffmpegjs memfs filesystem.
+     */
+    static _generateProjectImages = async (args) => {
+        
+      let { project, onProgress } = args;
 
-          // Increase frame number.
-          frameNumber += 1;
+      onProgress && onProgress('Rendering Images', 33);
 
-          // Add frame to frame list.
-          imageFiles.push(memfs_obj);
-        });
+      return new Promise( resolve => {
+          let imageData = [];
 
-        callback(imageFiles);
-      },
-    });
-  }
+          // Get frame images from project, send to the video worker.
+          let frameNumber = 0;
+          project.generateImageSequence({
+              imageType: 'image/jpeg',
 
-  static _generateAudioFiles (project, callback) {
-    project.generateAudioTrack({}, audioBuffer => {
-      if(!audioBuffer) {
-        callback([]);
-      } else {
-        var wavBuffer = toWav(audioBuffer);
-        let memfs_obj = {name: 'audiotrack.wav', data: new Uint8Array(wavBuffer)};
-        callback([memfs_obj]);
+              onProgress: (currentFrame, numTotalFrames) => {
+                let progress = 33 + (currentFrame/numTotalFrames) * 20;
+                onProgress('Rendering Frame ' + currentFrame + '/' + numTotalFrames, progress);
+              },
+
+              onFinish: (images) => {
+                // Load frame images into the web worker's memory
+                onProgress('Converting Frames' , 70);
+                images.forEach(image => {
+                // Create Name and array buffer of frame image.
+                let paddedNum = (frameNumber + '').padStart(12, '0');
+                let name = "frame" + paddedNum + ".jpeg";
+
+                // Get the base 64 value and convert it to an array buffer.
+                let cleanBase64 = image.src.split(',')[1];
+                let buffer = b64toBuff.decode(cleanBase64);
+
+                // Store name and buffer in memfs appropriate object.
+                imageData.push({name:name, data:new Uint8Array(buffer)});
+                
+                // Increase frame number.
+                frameNumber += 1;
+              });
+
+                resolve(imageData);
+              },
+          });
+      });
+    }
+
+    static _generateVideo = async ({images, audio, args}) => {
+      let { project, onProgress, onFinish, worker } = args;
+
+      onProgress("Rendering Final Video", 85);
+
+      for (let i=0; i<images.length; i++) {
+          let image = images[i]; 
+          let { name, data } = image;
+          await worker.write(name, data);
       }
-    });
-  }
 
-  static _generateVideoFile (args, callback) {
-    let project = args.project
-    this._generateImageFiles(args, imageFiles => {
-      args.onProgress("Combining Images into Video",33);
-      this._ffmpeg.onDone = videoData => {
-        let videoFiles = [{
-          name: 'video-no-sound.mp4',
-          data: new Uint8Array(videoData),
-        }];
-        callback(videoFiles);
-      };
+      if (audio) {
+          await worker.write('audio.wav', audio);
+      }
 
       // Slow down the video if the framerate is less than 6 (framerate <6 causes a corrupted video to render)
       let filterv = 'showinfo';
@@ -181,49 +105,44 @@ class VideoExport {
           filterv = 'setpts='+(6/project.framerate)+'*PTS, ' + filterv;
       }
 
-      let width = args.width || project.width;
-      let height = args.height || project.height;
+      let command = [
+          '-r', '' + Math.max(6, project.framerate),
+          '-f', 'image2',
+          '-s', project.width + "x" + project.height,
+          '-i', 'frame%12d.jpeg',
+          '-i', 'audio.wav',
+          '-vcodec', 'mpeg4',
+          '-q:v', '10', //10=good quality, 31=bad quality
+          '-filter:v', filterv,
+          'out.mp4',
+        ]
 
-      this._ffmpeg.run([
-        '-r', '' + Math.max(6, project.framerate),
-        '-f', 'image2',
-        '-s', width + "x" + height,
-        '-i', 'frame%12d.jpeg',
-        '-vcodec', 'mpeg4',
-        '-q:v', '10', //10=good quality, 31=bad quality
-        '-filter:v', filterv,
-        'out.mp4',
-      ], imageFiles, 'images_to_video');
-    });
-  }
+        await worker.run(command.join(" "), {output: 'out.mp4'});
+        const { data } = await worker.read('out.mp4');
 
-  static _mixAudioAndVideoFiles (project, videoFiles, audioFiles, callback) {
-    if(audioFiles.length === 0) {
-      callback(videoFiles[0].data);
-    } else {
-      let videoAndAudioFiles = audioFiles.concat(videoFiles);
-      this._ffmpeg.onDone = videoWithSoundData => {
-        callback(videoWithSoundData);
-      }
-      this._ffmpeg.run([
-        '-i', 'video-no-sound.mp4',
-        '-i', 'audiotrack.wav',
-        '-c:v', 'copy',
-        '-c:a', 'aac',
-        '-strict', '-2',
-        'out.mp4',
-      ], videoAndAudioFiles, 'add_audio_track');
+        // Remove Data from worker when done.
+        for (let i=0; i<images.length; i++) {
+          let image = images[i]; 
+          let { name } = image;
+          await worker.remove(name);
+        }
+
+        if (audio) {
+          await worker.remove('audio.wav', audio);
+        }
+
+        await worker.terminate();
+
+        if(!(data instanceof Uint8Array)) {
+          data = new Uint8Array(data);
+        }
+    
+        let blob = new Blob([data]);
+        onProgress("Video Render Complete", 100);
+        onFinish();
+
+        window.saveAs(blob, project.name+'.mp4');
     }
-  }
-
-  static _saveVideoFile (project, data) {
-    if(!(data instanceof Uint8Array)) {
-      data = new Uint8Array(data);
-    }
-
-    let blob = new Blob([data]);
-    window.saveAs(blob, project.name+'.mp4');
-  }
 }
 
 export default VideoExport;
