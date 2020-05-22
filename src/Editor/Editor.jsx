0b0cb8e1d167c0f3028d3a3e2fb8b93c3a1c32eb
Luca Damasco <luca.damasco@gmail.com>
2020-05-22 13:05:54 -0400

    Fix video renderer missing sounds on frame 1.

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -62,7 +62,7 @@ class Editor extends EditorCore {
     // "Live" editor states
     this.project = null;
     this.paper = null;
-    this.editorVersion = "1.17.1";
+    this.editorVersion = "1.17.2";
 
     // GUI state
     this.state = {
