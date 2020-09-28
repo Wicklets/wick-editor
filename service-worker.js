6cba5e4d0640fe28896b5687c10e8460ae1f8a21
Luca Damasco <luca.damasco@gmail.com>
2020-09-28 16:49:36 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.50c07f6a13bd703e0dc48045c7c7f13b.js"
+  "./precache-manifest.8527e1a200060c4a4f5f56d70355ce91.js"
 );
 
 workbox.clientsClaim();
