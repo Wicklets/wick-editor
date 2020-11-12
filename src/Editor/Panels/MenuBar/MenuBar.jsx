6154a61ed78fd38c95f29fa04840dbc0c40736fc
Luca Damasco <luca.damasco@gmail.com>
2020-11-12 14:43:19 -0500

    Center menu project title

--- src/Editor/Panels/MenuBar/MenuBar.jsx
+++ src/Editor/Panels/MenuBar/MenuBar.jsx
@@ -32,19 +32,21 @@ class MenuBar extends Component {
   renderDesktop = () => {
     return(
       <div className="docked-pane menu-bar" aria-label="Menu Bar">
-        <MenuBarIconButton
-          id="tool-information-button"
-          tooltip="Editor Information"
-          action={() => this.props.openModal('EditorInfo')}
-          icon="mascotmark"
-          />
+        <div className="menu-bar-info-container">
+          <MenuBarIconButton
+            id="tool-information-button"
+            tooltip="Editor Information"
+            action={() => this.props.openModal('EditorInfo')}
+            icon="mascotmark"
+            />
 
-        <MenuBarSupportButton
-          icon="redheart"
-          text="support us"
-          id="support-us-button"
-          action={() => this.props.openModal('SupportUs')}
-          />
+          <MenuBarSupportButton
+            icon="redheart"
+            text="support us"
+            id="support-us-button"
+            action={() => this.props.openModal('SupportUs')}
+            />
+          </div>
 
         <div className="menu-bar-project-name" role="button" onClick={() => this.props.openModal("SimpleProjectSettings")}>
           {this.props.projectName}
@@ -77,6 +79,7 @@ class MenuBar extends Component {
             icon="gear"
             action={() => this.props.openModal('SettingsModal')}
             tooltip="Editor Settings"
+            tooltipPlace="left"
             id="editor-settings-button" />
         </div>
       </div>
