51a021124ef29cd8fe536a3a827e9c5d59885d3a
Luca Damasco <luca.damasco@gmail.com>
2020-12-11 16:06:22 -0500

    Fix bug with downloadable editor where toolbar was obstructred

--- public/corelibs/wick-engine/wickengine.js
+++ public/corelibs/wick-engine/wickengine.js
@@ -1,5 +1,5 @@
 /*Wick Engine https://github.com/Wicklets/wick-engine*/
-var WICK_ENGINE_BUILD_VERSION = "2020.12.11.10.39.13";
+var WICK_ENGINE_BUILD_VERSION = "2020.12.11.15.25.29";
 /*!
  * Paper.js v0.12.4 - The Swiss Army Knife of Vector Graphics Scripting.
  * http://paperjs.org/
