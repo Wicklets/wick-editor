351bde7ef713bb931dff383aa64f304adabf35e9
Luca Damasco <luca.damasco@gmail.com>
2021-03-03 10:43:21 -0500

    Update WelcomeMessage.jsx

--- src/Editor/Modals/WelcomeMessage/WelcomeMessage.jsx
+++ src/Editor/Modals/WelcomeMessage/WelcomeMessage.jsx
@@ -44,8 +44,8 @@ class WelcomeModal extends Component {
     let t1 = ["Guy de Bree", "Dimp", "Hyun's Dojo"]
     let t2 = ["Constance Ye", "Dan Doggett", "Anonymous"]
     let t3 = ["Gautaum Bose", "Trevor", "Jovanny Rodriguez", "André Bray", "Colin Fitz-Gerald"]
-    let t4 = ["Golan Levin", "Daniel Sun", "Benjamin Briand", "Joseph Hocking", "Charisse Hampton", "Jessie Young", "Bluecake", "O.K. Keyes", "Sarksus", "Laurens Bonnema", "Anonymous"]
-    let t5 = ["Joe", "Karlin Fox", "StepSwitcher", "Kandy Kat", "Ann Griffin", "Happyships", "Dixie Dorward", "Albin Rodriguex"]
+    let t4 = ["Golan Levin", "Daniel Sun", "Benjamin Briand", "Joseph Hocking", "Charisse Hampton", "Jessie Young", "Bluecake", "O.K. Keyes", "Sarksus", "Laurens Bonnema", "Supersatanson", "Arturo Andreu Villalba", "Anonymous"]
+    let t5 = ["Joe", "Karlin Fox", "StepSwitcher", "Ann Griffin", "Happyships", "Dixie Dorward", "Albin Rodriguex", "Loren Rosser", "Brent Knopf", "Uhlmann", "Lisa McBride", "Lana Storto", "Justin James"]
     this.patreonSupporters = t1.concat(t2, t3, t4, t5)
   }
 
